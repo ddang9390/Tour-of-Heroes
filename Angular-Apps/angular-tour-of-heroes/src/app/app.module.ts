d5e8e21e1464f9b91cfdf3aac1fd35395f57b199
ddang9390 <dnld1ng@yahoo.com>
2019-05-14 13:49:46 -0400

    initial usage of http services

--- Angular-Apps/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular-Apps/angular-tour-of-heroes/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 @NgModule({
   declarations: [
     AppComponent,
