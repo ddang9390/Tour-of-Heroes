18d4bac3c94c9754a299e2ac9a98e61219f91d57
ddang9390 <dnld1ng@yahoo.com>
2019-05-14 14:01:50 -0400

    Replaced mock heroes with http services

--- Angular-Apps/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular-Apps/angular-tour-of-heroes/src/app/app.module.ts
@@ -25,7 +25,15 @@ import { InMemoryDataService } from './in-memory-data.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+ 
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
