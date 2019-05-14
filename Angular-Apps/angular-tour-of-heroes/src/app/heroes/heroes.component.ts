0a4d8f1c4899e440d951ce894df1314b467be3d2
ddang9390 <dnld1ng@yahoo.com>
2019-05-14 12:38:54 -0400

    Used routerLink for hero details and removed dead code

--- Angular-Apps/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular-Apps/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,13 +9,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
-
-  
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
 
   //subscribe method allows service to actually wait for server to make requests
   getHeroes(): void{
